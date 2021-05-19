a1c6f93ce9f9c155467abb7df11e5cb54828583a
Jeffrey Baker <jeffrey1871@gmail.com>
2021-05-18 20:25:45 -0500

    Update

--- DapperPracticeA/Program.cs
+++ DapperPracticeA/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Data;
+using System.IO;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+
 
 namespace DapperPracticeA
 {
@@ -6,7 +11,13 @@ namespace DapperPracticeA
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string connString = config.GetConnectionString("DefaultConnection");
+            IDbConnection conn = new MySqlConnection(connString);
         }
     }
 }
