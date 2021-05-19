fcf2b33f69e3847148af48983af5010a5a1b468b
Jeffrey Baker <jeffrey1871@gmail.com>
2021-05-18 19:53:57 -0500

    Add project files.

---
+++ DapperPracticeA/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DapperPracticeA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
