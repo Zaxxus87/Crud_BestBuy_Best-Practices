4591acd5a20ad05ed832c4f86da75ad887c2eafa
Jeffrey Baker <jeffrey1871@gmail.com>
2021-05-21 18:48:54 -0500

    Finished Project

--- DapperPracticeA/ProductRepo.cs
+++ DapperPracticeA/ProductRepo.cs
@@ -1,46 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dapper;
 
 namespace DapperPracticeA
 {
     class ProductRepo : IProductRepository
     {
-        public void DeleteProduct(int id)
-        {
-            throw new NotImplementedException();
-        }
+        private readonly IDbConnection _connection;
 
+        public ProductRepo(IDbConnection connection)
+        {
+            _connection = connection;
+        }
         public IEnumerable<Product> GetAllProducts()
         {
-            throw new NotImplementedException();
+            return _connection.Query<Product>("SELECT * FROM products;");
         }
-
         public Product GetProduct(int id)
         {
-            throw new NotImplementedException();
+            return _connection.QuerySingle<Product>("SELECT * FROM products WHERE ProductID = @id;",
+                new {id = id});
         }
 
         public void InsertProduct(string name, double price, int catID)
         {
-            throw new NotImplementedException();
+            _connection.Execute("INSERT INTO products (Name, Price, CategoryID, OnSale, StockLevel)" +
+                "VALUES (@name, @price, @categoryID, @onSale, @stockLevel);", 
+                new {name = name, price = price, categoryID = catID, OnSale = 0, stockLevel = '0'  });
         }
 
         public void UpdateProduct(int id, string newName, double newPrice, int newCatID, int newOnSale, string newSL)
         {
-            throw new NotImplementedException();
+            _connection.Execute("UPDATE products SET Name = @newName, Price = @newPrice, " +
+                "CategoryID = @newCatID, OnSale = @newOnSale, StockLevel = @newSL WHERE ProductID = @id;",
+                new {newName = newName, newPrice = newPrice, newCatID = newCatID, newOnSale = newOnSale, newSL = newSL, id = id });
         }
 
         public void UpdateProductPrice(int id, double newPrice)
         {
-            throw new NotImplementedException();
+            _connection.Execute("UPDATE products SET Price = @newPrice WHERE ProductID = @id;", 
+                new {id = id, newPrice = newPrice });
         }
 
         public void UpdateProductPrice(string name, double newPrice)
         {
-            throw new NotImplementedException();
+            _connection.Execute("UPDATE products SET Price = @newPrice WHERE Name = @name;",
+                new { name = name, newPrice = newPrice });
+        }
+        public void DeleteProduct(int id)
+        {
+            _connection.Execute("DELETE FROM products WHERE ProductID = @id;", 
+                new {id = id});
         }
     }
 }
--- DapperPracticeA/Program.cs
+++ DapperPracticeA/Program.cs
@@ -19,15 +19,33 @@ namespace DapperPracticeA
             string connString = config.GetConnectionString("DefaultConnection");
             IDbConnection conn = new MySqlConnection(connString);
 
-            var repo = new DepartmentRepo(conn);
+            //var repo1 = new DepartmentRepo(conn);
             //var departments = repo.GetAllDepartments();
             //foreach(var d in departments)
             //    Console.WriteLine($"{d.DepartmentID} - {d.Name}");
-            //Console.WriteLine(repo.GetDepartment(2).Name);
+            //Console.WriteLine(repo1.GetDepartment(2).Name);
 
-            //repo.InsertDepartment("New Department");
-            //repo.UpdateDepartment(6, "Renamed Dept");
-            repo.DeleteDepartment(6);
+            //repo1.InsertDepartment("New Department");
+            //repo1.UpdateDepartment(6, "Renamed Dept");
+            //repo1.DeleteDepartment(6);
+
+            var repo2 = new ProductRepo(conn);
+            var products = repo2.GetAllProducts();
+            foreach (var p in products)
+            {
+                Console.WriteLine($"{p.ProductID} - {p.Name}: {p.Price}/{p.StockLevel}");
+                if (p.OnSale == 1)
+                    Console.WriteLine("This product is on sale");
+                Console.WriteLine();
+            }
+
+            Console.WriteLine(repo2.GetProduct(41).Name);
+            //repo2.InsertProduct("TestProduct A", 10.99, 1);
+            //repo2.UpdateProduct(943, "TestProduct B", 15.99, 2, 1, "10");
+            //repo2.UpdateProduct(944, "TestProduct c", 99.99, 6, 1, "100");
+            //repo2.UpdateProductPrice(942, 18.99);
+            //repo2.UpdateProductPrice("TestProduct B", 30.00);
+            //repo2.DeleteProduct(944);
         }
     }
 }
